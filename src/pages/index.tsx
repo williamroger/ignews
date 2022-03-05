b02209ac993973755e824ef52d4e8a2b7184d1f0
William Roger <williamroger.frontend@gmail.com>
2022-03-04 23:47:28 -0300

    adicionado title dinâmico

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
+import Head from 'next/head';
+
 import styles  from  '../../styles/home.module.scss';
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>Hello World</h1>
+    <>
+      <Head>
+        <title>ínicio | ig.news</title>
+      </Head>
+      <h1 className={styles.title}>Hello World</h1>
+    </>
   )
 }
