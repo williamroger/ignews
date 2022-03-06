ce95b23dd1c1eb9abfc839d6049d34deddef295a
William Roger <williamroger.frontend@gmail.com>
2022-03-06 00:53:15 -0300

    adicionado componente Header

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,15 @@
-import Document, { Html, Head, Main, NextScript} from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
-  render() {
+	render() {
 		return (
 			<Html>
 				<Head>
 					<link rel="preconnect" href="https://fonts.googleapis.com" />
 					<link rel="preconnect" href="https://fonts.gstatic.com" />
 					<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
+
+					<link rel="shortcut icon" href="/favicon.png" type="image/png" />
 				</Head>
 				<body>
 					<Main />
@@ -15,5 +17,5 @@ export default class MyDocument extends Document {
 				</body>
 			</Html>
 		);
-	} 
+	}
 }
