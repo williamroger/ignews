625c253aad912ff10ea7cc3e5db7fb4a501b9d1f
William Roger <williamroger.frontend@gmail.com>
2022-03-08 22:27:08 -0300

    adicionado autenticação com Next Auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -2,23 +2,26 @@ import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 
 import styles from './styles.module.scss';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
-
-  return isUserLoggedIn ? (
+  const { data: session } = useSession();
+  console.log('session ', session);
+  return session ? (
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signOut()}
     >
       <FaGithub color="#04D361" />
-      William Roger
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#EBA417" />
       Sign in with GitHub
