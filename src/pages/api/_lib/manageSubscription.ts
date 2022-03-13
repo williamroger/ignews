38780cc9fb2105f555c53853121ab6ac99dc5e88
William Roger <williamroger.frontend@gmail.com>
2022-03-13 01:51:54 -0300

    Salvando dados do Evento

---
+++ src/pages/api/_lib/manageSubscription.ts
@@ -0,0 +1,38 @@
+import { query as q } from 'faunadb';
+import { fauna } from "../../../services/fauna";
+import { stripe } from '../../../services/stripe';
+
+export async function saveSubscription(
+  subscriptionId: string,
+  customerId: string,
+) {
+  const userRef = await fauna.query(
+    q.Select(
+      'ref',
+      q.Get(
+        q.Match(
+          q.Index('user_by_stripe_customer_id'),
+          customerId
+        )
+      )
+    )
+  );
+
+  // pega todos os dados de um subscrição não apenas o ID.
+  const subscription = await stripe.subscriptions.retrieve(subscriptionId);
+
+  // pega apenas os dados que necessitam ser salvos no banco de dados.
+  const subscriptionData = {
+    id: subscription.id,
+    userId: userRef,
+    status: subscription.status,
+    price_id: subscription.items.data[0].price.id,
+  };
+
+  await fauna.query(
+    q.Create(
+      q.Collection('subscriptions'),
+      { data: subscriptionData }
+    )
+  );
+}
