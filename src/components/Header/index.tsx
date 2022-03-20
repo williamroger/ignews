06ea148039692b0220d1ba046224e96d765a6eb8
William Roger <williamroger.frontend@gmail.com>
2022-03-20 17:30:45 -0300

    adicionada navegação na aplicação

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import { SignInButton } from '../SignInButton';
+import Link from 'next/link';
 
 import styles from './styles.module.scss';
 
@@ -8,8 +9,12 @@ export function Header() {
 			<div className={styles.headerContent}>
 				<img src="/images/logo.svg" alt="ig.news" />
 				<nav>
-					<a className={styles.active}>Home</a>
-					<a>Posts</a>
+					<Link href="/">
+						<a className={styles.active}>Home</a>
+					</Link>
+					<Link href="/posts">
+						<a>Posts</a>
+					</Link>
 				</nav>
 
 				<SignInButton />
